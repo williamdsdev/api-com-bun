f925218f1777d70bfdb2cfd5ae4e67b1db5913cc
William Desteffani Soares <williamds.uchiha@gmail.com>
2025-02-05 22:57:40 -0300

    Configurando o Drizzle ORM

---
+++ src/db/schema/restaurants.ts
@@ -0,0 +1,10 @@
+import { serial, text, timestamp, pgTable, pgEnum } from "drizzle-orm/pg-core";
+import { createId } from "@paralleldrive/cuid2";
+
+export const restaurants = pgTable("restaurants", {
+    id: text("id").$defaultFn(() => createId()).primaryKey(),
+    name: text("name").notNull(),
+    description: text("description"),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at").notNull().defaultNow(),
+})
