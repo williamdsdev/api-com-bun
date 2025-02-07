68a8520e473ab14cf35908ff75381ba92e621903
William Desteffani Soares <williamds.uchiha@gmail.com>
2025-02-06 21:44:21 -0300

    Relacionamentos no Drizzle

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -3,7 +3,7 @@ import { createId } from "@paralleldrive/cuid2";
 
 export const userRoleEnum = pgEnum('user_role', ['manager', 'customer'])
 
-export const user = pgTable("users", {
+export const users = pgTable("users", {
     id: text("id").$defaultFn(() => createId()).primaryKey(),
     name: text("name").notNull(),
     email: text("email").notNull().unique(),
@@ -11,4 +11,4 @@ export const user = pgTable("users", {
     role: userRoleEnum('role').default('customer').notNull(),
     createdAt: timestamp("created_at").notNull().defaultNow(),
     updatedAt: timestamp("updated_at").notNull().defaultNow(),
-})
+});
