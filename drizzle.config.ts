a1eca38c0ae640a3a519c6883f6bbf9a43cbd1ea
William Desteffani Soares <williamds.uchiha@gmail.com>
2025-02-15 17:45:11 -0300

    Variáveis ambiente no Bun

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,11 @@
 import type { Config } from "drizzle-kit";
+import { env } from "./src/env";
 
 export default {
   schema: "./src/db/schema/index.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    url: 'postgresql://docker:docker@localhost:5432/pizzashop',
+    url:env.DATABASE_URL,
   }
 } satisfies Config;
