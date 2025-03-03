085dc3e625ec144d043831b0bc738ec8f1264f17
William Desteffani Soares <williamds.uchiha@gmail.com>
2025-03-02 23:00:05 -0300

    Configurando ESLint no projeto

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,8 +1,8 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-import * as schema from "./schema";
-import { env } from "../env";
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+import * as schema from './schema'
+import { env } from '../env'
 
-const connection = postgres(env.DATABASE_URL);
+const connection = postgres(env.DATABASE_URL)
 
-export const db = drizzle(connection, { schema });
+export const db = drizzle(connection, { schema })
--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,66 +1,71 @@
-import { faker } from "@faker-js/faker";
-import { users, restaurants } from "./schema";
-import { db } from "./connection";
-import chalk from "chalk";
+/* eslint-disable drizzle/enforce-delete-with-where */
+
+import { faker } from '@faker-js/faker'
+import { users, restaurants } from './schema'
+import { db } from './connection'
+import chalk from 'chalk'
 
 /**
  * Reset database
  */
 
-await db.delete(users);
-await db.delete(restaurants);
+await db.delete(users)
+await db.delete(restaurants)
 
-console.log(chalk.yellow("✅ Database reset!"));
+console.log(chalk.yellow('✅ Database reset!'))
 
 /**
  * Create customers
  */
 
 await db.insert(users).values([
-    {
-        name: faker.person.fullName(),
-        email: faker.internet.email(),
-        role: "customer",
-    },
-    {
-        name: faker.person.fullName(),
-        email: faker.internet.email(),
-        role: "customer",
-    },
-]);
+  {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    role: 'customer',
+  },
+  {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    role: 'customer',
+  },
+])
 
-console.log(chalk.yellow("✅ Created customers!"));
+console.log(chalk.yellow('✅ Created customers!'))
 
 /**
  * Create manager
  */
 
-const [manager] = await db.insert(users).values([
+const [manager] = await db
+  .insert(users)
+  .values([
     {
-        name: faker.person.fullName(),
-        email: "admin@admin.com",
-        role: "manager",
+      name: faker.person.fullName(),
+      email: 'admin@admin.com',
+      role: 'manager',
     },
-]).returning({
+  ])
+  .returning({
     id: users.id,
-});
+  })
 
-console.log(chalk.yellow("✅ Created manager!"));
+console.log(chalk.yellow('✅ Created manager!'))
 
 /**
  * Create restaurant
  */
 
 await db.insert(restaurants).values([
-    {
-        name: faker.company.name(),
-        description: faker.lorem.paragraph(),
-        managerId: manager.id,
-    },
-]);
+  {
+    name: faker.company.name(),
+    description: faker.lorem.paragraph(),
+    managerId: manager.id,
+  },
+])
 
-console.log(chalk.yellow("✅ Created restaurant!"));
+console.log(chalk.yellow('✅ Created restaurant!'))
 
-console.log(chalk.greenBright("Database seeded successfully!"));
+console.log(chalk.greenBright('Database seeded successfully!'))
 
-process.exit();
+process.exit()
