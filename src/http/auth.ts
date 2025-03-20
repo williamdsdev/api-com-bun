d3f162572be617d06842e3b868538b3422c4e145
William Desteffani Soares <williamds.uchiha@gmail.com>
2025-03-19 21:07:03 -0300

    Rota: Logout e derivação

--- src/http/auth.ts
+++ src/http/auth.ts
@@ -1,16 +1,32 @@
-import cookie from '@elysiajs/cookie'
 import jwt from '@elysiajs/jwt'
-import Elysia, { t } from 'elysia'
+import Elysia, { t, type Static } from 'elysia'
 import { env } from '../env'
+
+const jwtPayload = t.Object({
+  sub: t.String(),
+  restaurantId: t.Optional(t.String()),
+})
 
 export const auth = new Elysia()
   .use(
     jwt({
       secret: env.JWT_SECRET_KEY,
-      schema: t.Object({
-        sub: t.String(),
-        restaurantId: t.Optional(t.String()),
-      }),
+      schema: jwtPayload,
     }),
   )
-  .use(cookie())
+  .derive({ as: 'scoped' }, ({ jwt, cookie: { auth } }) => {
+    return {
+      signUser: async (payload: Static<typeof jwtPayload>) => {
+        const token = await jwt.sign(payload)
+
+        auth.value = token
+        auth.httpOnly = true
+        auth.maxAge = 60 * 60 * 24 * 7 // 7 days
+        auth.path = '/'
+      },
+
+      signOut: async () => {
+        auth.remove()
+      },
+    }
+  })
--- src/http/routes/authenticate-from-link.ts
+++ src/http/routes/authenticate-from-link.ts
@@ -7,7 +7,7 @@ import { eq } from 'drizzle-orm'
 
 export const authenticateFromLink = new Elysia().use(auth).get(
   '/auth-links/authenticate',
-  async ({ query, jwt, cookie: { auth }, set }) => {
+  async ({ query, signUser, set }) => {
     const { code, redirect } = query
 
     const authLinkFromCode = await db.query.authLinks.findFirst({
@@ -35,16 +35,9 @@ export const authenticateFromLink = new Elysia().use(auth).get(
       },
     })
 
-    const token = await jwt.sign({
+    await signUser({
       sub: authLinkFromCode.userId,
       restaurantId: managedRestaurant?.id,
-    })
-
-    auth.set({
-      httpOnly: true,
-      maxAge: 60 * 60 * 24 * 7, // 7 days
-      path: '/',
-      value: token,
     })
 
     await db.delete(authLinks).where(eq(authLinks.code, code))
